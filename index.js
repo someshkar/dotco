8e269352382a86e48b716fdc0445a9d7ba701a0c
DarshSikka <67545017+DarshSikka@users.noreply.github.com>
2021-06-19 19:13:53 +0530

    404 Page Addition (#7)
    * Add 404
    
    * Changes made
    
    * 404 Page
    
    * Change UI
    
    Co-authored-by: KAPIL SIKKA <kapilsikka@KAPILs-Air.Dlink>

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const fetch = require('node-fetch')
-
+const fs=require("fs");
+const path=require("path");
+const bodyParser=require("body-parser");
 const app = express()
 const { getTarget } = require('./lib/airtable')
-
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.get('/', async (req, res) => {
-  try {
+  try { 
     return res.redirect(302, process.env.MAIN_DOMAIN)
   } catch (e) {
-    console.error(e)
+    console.error(e) 
   }
 })
 
@@ -20,18 +23,20 @@ app.get('/gh/:repo', async (req, res) => {
 
     const { ok: repoExists } = await fetch(
       `https://api.github.com/repos/${ghUsername}/${repo}`
-    )
+    ) 
 
     if (repoExists) {
       return res.redirect(302, `https://github.com/${ghUsername}/${repo}`)
     }
-
-    return res.status(404).send('404')
+    let page=fs.readFileSync(__dirname+ '/views/notfound.html', 'utf8');
+    page=page.replace("tochange", "");
+    console.log(page); 
+    res.status(404).send(page)
+    return
   } catch (e) {
     console.error(e)
   }
 })
-
 // Main slugs
 app.get('/*', async (req, res) => {
   try {
@@ -42,12 +47,18 @@ app.get('/*', async (req, res) => {
       return res.redirect(302, target)
     }
 
-    return res.status(404).send('404')
+    let page=fs.readFileSync(__dirname+ '/views/notfound.html', 'utf8');
+    page=page.replace("tochange", req.url);
+    console.log(page); 
+    res.status(404).send(page);
   } catch (e) {
     console.error(e)
   }
 })
-
+app.post("/*", (req, res)=>{
+  const {new_adress}=req.body;
+  res.redirect(`${new_adress}`)
+})
 const port = process.env.PORT || 3000
 app.listen(port, (err) => {
   console.log(`Shortener listening on ${port}!`)
